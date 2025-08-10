7f4dd3d773c8564b8a2fe5e89c22141b05ca8f7f
rich <schramm.r@gmail.com>
2025-08-10 13:31:22 -0700

    Bump version to 6.2.2

--- global.R
+++ global.R
@@ -1,7 +1,7 @@
 ######## I put github release version and other data here ############
 ######## so it can be displayed on the footer of the kiosk window ####
-gblFooterText <- "MOTUS_KIOSK vsn 6.2.1  26-May-2025"
-gblUserAgentText <- "MOTUS_KIOSK vsn 6.2.1"
+gblFooterText <- "MOTUS_KIOSK vsn 6.2.2  10-Aug-2025"
+gblUserAgentText <- "MOTUS_KIOSK vsn 6.2.2"
 mytoggle<<-FALSE #useful global variable for debugging
 ###############################################################################
 # Copyright 2022-2023 Richard Schramm
