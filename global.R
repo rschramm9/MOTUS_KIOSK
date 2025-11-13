dc78275520607dcac5763a06a444f5589d2e039e
rich <schramm.r@gmail.com>
2025-11-13 14:07:39 -0800

    bump version number and date

--- global.R
+++ global.R
@@ -1,6 +1,6 @@
 ######## I put github release version and other data here ############
 ######## so it can be displayed on the footer of the kiosk window ####
-gblFooterText <- "MOTUS_KIOSK vsn 6.2.4  10-Nov-2025"
+gblFooterText <- "MOTUS_KIOSK vsn 6.2.4  13-Nov-2025"
 gblUserAgentText <- "MOTUS_KIOSK vsn 6.2.4"
 mytoggle<<-FALSE #useful global variable for debugging
 ###############################################################################
